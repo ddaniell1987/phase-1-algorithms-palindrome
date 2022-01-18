4ebaa9288eb1e8b72c23cca35c16b289d7fd8bef
DominiqueFlorence <ddaniell1987@gmail.com>
2022-01-18 15:16:47 -0500

    commit tree 97c57ab2fe9cca5d47fff70142099d48a933d3a9

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+  const length = string.length;
+    for(let i = 0; i < length / 2; i++) {
+      if(string[i] !== string[length - 1 - i]) {
+    return false;
+  }
 }
+return true;
+}
+/*const string = prompt("Enter a string: ");
+const value = isPalindrome(string);
+console.log(value);*/
 
 /* 
   Add your pseudocode here
+  Prompt to enter the word. 
+  Write a function that will receive one argument a string. The function should be named isPalindrome.
+  The function should only deal with lowercase strings.
+  I will need a variable for the palindrome.
+  It will return true if the string is a palindrome.
+  It will return false if not a palindrome
+  
+
+
 */
 
 /*
